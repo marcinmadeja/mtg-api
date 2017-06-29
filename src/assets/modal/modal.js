e6d7f732adadbe5b8f09defed7cc0d55cd804e61
jonelet <jonelet@gmail.com>
2017-06-29 19:14:42 +0200

    modal: refactoring

--- src/assets/modal/modal.js
+++ src/assets/modal/modal.js
@@ -1,23 +1,48 @@
 const modal = (function () {
   const settings = {
     initFromStart: true,
+    animationName: 'md-modal--effect-1',
+    modalClass: 'md-modal',
+    bodyClass: 'md-modal--open',
     showModalClass: 'md-modal--show',
     overlayClass: 'md-modal__overlay',
     triggerButtonData: 'data-md-modal',
     triggerBtnDataset: 'mdModal',
+    closeBtn: 'md-modal__btn-close',
   };
 
-  function openModal(id) {
+  function removeOverlay() {
+    document.querySelectorAll(`.${settings.overlayClass}`).forEach(overlay => {
+      overlay.parentNode.removeChild(overlay);
+    });
+  }
 
+  function addOverlay() {
+    removeOverlay();
+    const existingOverlay = document.querySelector(`.${settings.overlayClass}`);
+    const overlay = document.createElement('div');
+    overlay.classList.add(settings.overlayClass);
+    document.body.appendChild(overlay);
+  }
+
+  function openModal(modalId) {
+    const modalElement = document.getElementById(modalId);
+    if (!modalId) return false;
+    
+    modalElement.classList.add(settings.showModalClass);
+    document.body.classList.add(settings.bodyClass);
+    modalElement.style.display = 'block';
+    setTimeout(() => {
+      modalElement.classList.add(settings.animationName);
+    }, 50);
+
+    addOverlay();
   }
 
   function onButtonClick(e) {
     e.preventDefault();
     const modalId = this.dataset[settings.triggerBtnDataset];
-    const modalElement = document.getElementById(modalId);
-    if (!modalId || !modalElement) return false;
-    
-    modalElement.classList.add(settings.showModalClass);
+    openModal(modalId);
   }
 
   function initAllButtons() {
@@ -32,18 +57,40 @@ const modal = (function () {
     if (!visibleModal) return false;
 
     visibleModal.classList.remove(settings.showModalClass);
+    document.body.classList.remove(settings.bodyClass);
+    visibleModal.classList.remove(settings.animationName);
+  
+    setTimeout(() => {
+      visibleModal.style.display = 'none';    
+    }, 400);
+
+    removeOverlay();
+  }
+
+  function clickModalContent(e) {   
+    if (this === e.target) {
+      closeModal();
+    }
   }
 
   function init() {
     if (settings.initFromStart) initAllButtons();
 
-    document.querySelector(`.${settings.overlayClass}`).addEventListener('click', closeModal);
+    document.querySelectorAll(`.${settings.closeBtn}`).forEach(button => {
+      button.addEventListener('click', closeModal);
+    });
+
+    document.querySelectorAll('.md-modal').forEach(modalElement => {
+      modalElement.addEventListener('click', clickModalContent);
+    });
   }
 
   init();
 
   return {
     init,
+    closeModal,
+    openModal,
   };
 }());
 
